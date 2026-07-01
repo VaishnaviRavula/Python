e3bb4ba19e4755a768162ce27ad8863cd8833130
Ravula Vaishnavi <vaishnavireddyravula2006@gmail.com>
2026-07-01 18:09:32 +0530

    Add files via upload

---
+++ Indexing.py
@@ -0,0 +1,62 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+a='hyderabad'
+a[0]+a[1]+a[2]+a[3]+a[4]+a[5]
+'hydera'
+a[0]
+'h'
+a='i am in class'
+a[0]
+'i'
+a[1]
+' '
+a[2]+a[3]
+'am'
+>>> a[5]+a[6]
+'in'
+>>> a[8]+a[9]+a[10]+a[11]+a[12]
+'class'
+>>> 
+>>> a='simple is better than complex'
+>>> a[10]+a[11]+a[12]+a[13]+a[14]+a[15]
+'better'
+>>> a[22]+a[23]+a[24]+a[25]+a[26]+a[27]+a[28]
+'complex'
+>>> a[0]+a[1]+a[2]+a[3]+a[4]+a[5]
+'simple'
+>>> 
+>>> a='codegnan it solutions'
+>>> a[12]+a[13]+a[14]+a[15]+a[16]+a[17]+a[18]+a[19]+a[20]
+'solutions'
+>>> a[0]+a[1]+a[2]+a[3]+a[4]+a[5]+a[6]+a[7]
+'codegnan'
+>>> a[9]+a[10]
+'it'
+>>> 
+>>> a='i am learning pyhton'
+>>> a[-6]+a[-5]+a[-4]+a[-3]+a[-2]+a[-1]
+'pyhton'
+>>> a[-15]+a[-14]+a[-13]+a[-12]+a[-11]+a[-10]+a[-9]+a[-8]
+'learning'
+>>> a[-19]
+' '
+>>> a[20]
+Traceback (most recent call last):
+  File "<pyshell#24>", line 1, in <module>
+    a[20]
+IndexError: string index out of range
+>>> a[-20]
+'i'
+>>> a[-15]+a[-14]+a[-13]+a[-12]+a[-11]
+'learn'
+>>> 
+>>> b='Python Fullstack'
+>>> b[-5]+b[-4]+b[-3]+b[-2]+b[-1]
+'stack'
+>>> b[-6]+b[-7]+b[-8]+b[-9]
+'lluF'
+>>> b[-9]+b[-8]+b[-7]+b[-6]
+'Full'
+>>> b[-16]+b[-15]+b[-14]+b[-13]+b[-12]+b[-11]
+'Python'
+>>> 
