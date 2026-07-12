f243cf5b9274dfb726d0f0ce6161bd91a7e7b3e2
Ravula Vaishnavi <vaishnavireddyravula2006@gmail.com>
2026-07-12 17:39:38 +0530

    Add files via upload

---
+++ runtime.py
@@ -0,0 +1,84 @@
+#run time input()
+'''a=int(input("enter the a value:"))
+b=int(input("enter the b value:"))
+print(a+b)'''
+
+'''a=float(input("enter a value:"))
+b=float(input("enter b value:"))
+print(a+b)'''
+
+
+'''a=str(input("data 1"))
+b=str(input("data 2"))
+print(a+b)'''
+
+'''a=input("data 1")
+b=input("data2")
+print(a+b)'''
+
+'''fname=input("first name")
+lname=input("last name")
+print((fname+" "+lname).title())'''
+
+
+'''a=complex(input("enter a value"))
+b=complex(input("enter b value"))
+print(a+b)'''
+
+
+'''a=bool(input("enter the a value"))
+b=bool(input("enter the b value"))
+c=bool(input("enter the c value"))
+print(a+b+c)'''
+
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+option=int(input("choose the option 1.add 2.sub 3.mul"))
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+option=int(input(choose the option
+                 1.add
+                 2.sub
+                 3.mul))
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+
+'''a=int(input())
+b=int(input())
+print(a+b)'''
+
+#swapping of two variables
+'''a=int(input())
+b=int(input())
+a,b=b,a
+print(a,b)'''
+
+'''a=10
+b=20
+temp=a
+a=b
+b=temp
+print(a,b)'''
+
+'''a=10
+b=20
+a=a+b
+b=a-b
+a=a-b
+print(a,b)'''
+
+
+'''a=10
+b=20
+a=a+b
+b=a-b
+a=a-b
+print("after swapping a=%d,b=%d" %(a,b))'''
+
