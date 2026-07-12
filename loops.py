7e08f18b4c621e37938055c5c9ac6bd1c329cd31
Ravula Vaishnavi <vaishnavireddyravula2006@gmail.com>
2026-07-12 18:11:32 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,124 @@
+#loops
+#for,while,range,break,continue,pass
+
+#for loop()
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)'''
+
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(a)'''
+
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i,end=" ")'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+
+'''a=(4,5,6,7,8,9)
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+
+'''a={4,5,6,7,8,9}
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+
+'''d={"year":2026,"month":"july","date":10}
+for i in d:
+    print(i)
+for i in d.keys():
+    print(i)
+    print(type(d))
+    print(type(i))
+for i in d.values():
+    print(i)
+    print(type(d))
+    print(type(i))
+for i in d.items():
+    print(i)
+    print(type(d))
+    print(type(i))'''
+
+
+'''a=[True,False]
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+
+a=["APPLE","BANANA","MANGO"]
+'''b=str(a)
+print(b.upper())'''
+
+'''for i in a:
+    print(i.upper(),end=",")'''
+
+'''b=[]
+for i in a:
+    b.append(i.upper())
+print(b)'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
